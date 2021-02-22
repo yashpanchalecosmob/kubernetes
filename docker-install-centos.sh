6e744354be093c003dd6fc47170deebed4bbda17
yashpanchalecosmob <yash.panchal@ecosmob.com>
2021-02-22 15:55:32 +0530

    Docker script for centos

---
+++ docker-install-centos.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+
+sudo yum-config-manager --add-repo \
+  https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum update -y && sudo yum install -y \
+  containerd.io-1.2.13 \
+  docker-ce-19.03.11 \
+  docker-ce-cli-19.03.11
+
+sudo mkdir -p /etc/docker
+
+
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ]
+}
+EOF
+
+sudo mkdir -p /etc/systemd/system/docker.service.d
+
+sudo systemctl daemon-reload
+
+sudo systemctl enable docker
+sudo systemctl restart docker
+
+
+
+
