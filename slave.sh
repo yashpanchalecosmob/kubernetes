f376215580c66b17155c8dfe7b67fffa2054c808
yashpanchalecosmob <yash.panchal@ecosmob.com>
2021-02-25 13:33:32 +0530

    slave added

--- slave.sh
+++ slave.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
+
 setenforce 0
 sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
 
-firewall-cmd --permanent --add-port=6443/tcp
-firewall-cmd --permanent --add-port=2379-2380/tcp
+
 firewall-cmd --permanent --add-port=10250/tcp
-firewall-cmd --permanent --add-port=10251/tcp
-firewall-cmd --permanent --add-port=10252/tcp
-firewall-cmd --permanent --add-port=102525/tcp
 firewall-cmd --permanent --add-port=10255/tcp
+firewall-cmd --permanent --add-port=30000-32767/tcp
+firewall-cmd --permanent --add-port=6783/tcp
 firewall-cmd --reload
 modprobe br_netfilter
 echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
@@ -26,30 +25,10 @@ gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
 EOF
 
 
-
 yum install kubeadm --nogpgcheck -y
+
 
 systemctl restart docker && systemctl enable docker
 systemctl restart kubelet && systemctl enable kubelet
-
-kubeadm init
-
-
-
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
-
-
-
-export kubever=$(kubectl version | base64 | tr -d '\n')
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$kubever"
-
-kubectl get nodes
-kubectl get pods --all-namespaces
-kubectl cluster-info
-
-
-
 
 
